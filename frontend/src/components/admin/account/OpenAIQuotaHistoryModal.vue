ff00e3b1d7ee684099b890036c58c43bd930bc6b
fyf <fyf980921@gmail.com>
2026-09-07 17:23:58 +0800

    Use animated bar widths when adjusting quota chart gaps

--- frontend/src/components/admin/account/OpenAIQuotaHistoryModal.vue
+++ frontend/src/components/admin/account/OpenAIQuotaHistoryModal.vue
@@ -100,9 +100,11 @@ const quotaBarGapPlugin: Plugin<'bar'> = {
     for (let index = 0; index < count; index++) {
       const leftBar = leftMeta.data[index] as BarElement
       const rightBar = rightMeta.data[index] as BarElement
+      const leftWidth = leftBar.getProps(['width'], true).width
+      const rightWidth = rightBar.getProps(['width'], true).width
       const groupCenter = (leftBar.x + rightBar.x) / 2
-      leftBar.x = groupCenter - GROUPED_BAR_GAP / 2 - leftBar.width / 2
-      rightBar.x = groupCenter + GROUPED_BAR_GAP / 2 + rightBar.width / 2
+      leftBar.x = groupCenter - GROUPED_BAR_GAP / 2 - leftWidth / 2
+      rightBar.x = groupCenter + GROUPED_BAR_GAP / 2 + rightWidth / 2
     }
   }
 }
